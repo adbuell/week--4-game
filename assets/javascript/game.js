99ec744871f4e135521e2cc6ca923ac56557fb12
April Buell <adb701@gmail.com>
2017-12-16 16:59:52 -0700

    hola

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,3 +1,4 @@
+window.onload = function () {
 var wins = 0;
 var loses = 0;
 var randomNumber = Math.floor((math.random() * 120) = 19);
@@ -5,6 +6,10 @@ var yourNumber = 0;
 
 
 function myFunction() {
-    var x = Math.floor((Math.random() * 12) + 1);
-    document.getElementById("demo").innerHTML = x;
+    var blue = Math.floor((Math.random() * 12) + 1);
+    document.getElementById("blue").innerHTML = blue;
+};
+myFunction(); 
+
+
 }
