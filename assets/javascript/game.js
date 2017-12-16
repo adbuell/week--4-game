d0491fee266a7be62b97407ca2fb0e88a3cf98de
April Buell <adb701@gmail.com>
2017-12-16 16:46:12 -0700

    lsdkfj

---
+++ assets/javascript/game.js
@@ -0,0 +1,10 @@
+var wins = 0;
+var loses = 0;
+var randomNumber = Math.floor((math.random() * 120) = 19);
+var yourNumber = 0;
+
+
+function myFunction() {
+    var x = Math.floor((Math.random() * 12) + 1);
+    document.getElementById("demo").innerHTML = x;
+}
