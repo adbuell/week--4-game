6f3dc4e7d9e0163b86f622a7b6dbeffa5fb03b22
April Buell <adb701@gmail.com>
2017-12-16 20:49:31 -0700

    slkd

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -8,27 +8,58 @@ document.getElementById("randomnumber").innerHTML = randomNumber;
 
 document.getElementById("yournumber").innerHTML = yourNumber;
 
-function myFunction() {
+function check() {
+  if ($("yourNumber").text()==$("randomNumber").text()) {
+  wins += 1
+  $("#wins").text(wins);
+  }
+  else if ($("yourNumber").text()>$("randomNumber").text()) {
+  loses += 1
+  $("#loses").text(loses);
+  }
+}
+
+$("#blue").click(function(){
+   $("#yournumber").text(parseInt($("#blueval").text()) + parseInt($("#yournumber").text()));
+   check();
+ });
+
+ $("#green").click(function(){
+    $("#yournumber").text(parseInt($("#greenval").text()) + parseInt($("#yournumber").text()));
+    check();
+  });
+
+  $("#red").click(function(){
+     $("#yournumber").text(parseInt($("#redval").text()) + parseInt($("#yournumber").text()));
+     check();
+   });
+
+   $("#yellow").click(function(){
+      $("#yournumber").text(parseInt($("#yellowval").text()) + parseInt($("#yournumber").text()));
+      check();
+    });
+
+function blueFunction() {
     var blue = Math.floor((Math.random() * 12) + 1);
-    document.getElementById("blueval").innerHTML = blue;
+    $("#blueval").text(blue);
 };
-myFunction();
+blueFunction();
 
 function greenFunction() {
     var green = Math.floor((Math.random() * 12) + 1);
-    document.getElementById("greenval").innerHTML = green;
+    $("#greenval").text(green);
 };
 greenFunction();
 
 function redFunction() {
     var red = Math.floor((Math.random() * 12) + 1);
-    document.getElementById("redval").innerHTML = red;
+    $("#redval").text(red);
 };
 redFunction();
 
 function yellowFunction() {
     var yellow = Math.floor((Math.random() * 12) + 1);
-    document.getElementById("yellowval").innerHTML = yellow;
+    $("#yellowval").text(yellow);
 };
 yellowFunction();
 
