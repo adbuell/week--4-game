e7cce98f9abbe631f7a85f4ae6e8b652e321593c
April Buell <adb701@gmail.com>
2017-12-16 17:54:14 -0700

    sldkfj

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,15 +1,35 @@
 window.onload = function () {
 var wins = 0;
 var loses = 0;
-var randomNumber = Math.floor((math.random() * 120) = 19);
+var randomNumber = Math.floor(Math.random() * 101) + 19;
 var yourNumber = 0;
 
+document.getElementById("randomnumber").innerHTML = randomNumber;
+
+document.getElementById("yournumber").innerHTML = yourNumber;
 
 function myFunction() {
     var blue = Math.floor((Math.random() * 12) + 1);
-    document.getElementById("blue").innerHTML = blue;
+    document.getElementById("blueval").innerHTML = blue;
 };
-myFunction(); 
+myFunction();
 
+function greenFunction() {
+    var green = Math.floor((Math.random() * 12) + 1);
+    document.getElementById("greenval").innerHTML = green;
+};
+greenFunction();
 
-}
+function redFunction() {
+    var red = Math.floor((Math.random() * 12) + 1);
+    document.getElementById("redval").innerHTML = red;
+};
+redFunction();
+
+function yellowFunction() {
+    var yellow = Math.floor((Math.random() * 12) + 1);
+    document.getElementById("yellowval").innerHTML = yellow;
+};
+yellowFunction();
+
+};
